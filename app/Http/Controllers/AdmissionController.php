4cde4696ee75f3e012e516ca0e4ca573f4f07c9a
amedeopro <amedeopro@me.com>
2019-03-05 07:52:19 +0100

    Creato il form

---
+++ app/Http/Controllers/AdmissionController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdmissionController extends Controller
+{
+    public function index(){
+      return view('admission.index');
+    }
+
+    public function save(Request $request){
+      $data = $request->all();
+    }
+}
